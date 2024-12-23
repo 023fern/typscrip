b6f76acde17b8725a76b6c00409fc58974380a5d
Wuttha <Wuttha@webmail.npru.ac.th>
2024-12-23 11:58:55 +0700

    typs2312

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const {Author, Book} = require("./Ex1");
+console.log("############## Ex1 #############");
+const author1 = new Author("Worachet Uttha", "wuttha@webmail.npru.ac.th");
+console.log(author1.toString());
+const author2 = new Author ("Udsanee Pakdeetrakulwong", "udsanee@webmail.npru.ac.th");
+console.log(author2.toString());
+console.log("#############################");
